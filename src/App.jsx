e036f30a76735a902806ff29fae961a33f5f0734
JISU KIM <97648027+jijisusu3@users.noreply.github.com>
2025-08-08 14:59:31 -0400

    Add gh-pages config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Research from "./pages/Research";
 import ResearchDetail from "./pages/ResearchDetail";
@@ -9,12 +14,13 @@ function App() {
     <Router>
       <div className="bg-white text-white min-h-screen font-sans">
         <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/research" element={<Research />} />
-          <Route path="/research/:id" element={<ResearchDetail />} />
-        </Routes>
+        <BrowserRouter basename="/donggu-lee-portfolio">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/research" element={<Research />} />
+            <Route path="/research/:id" element={<ResearchDetail />} />
+          </Routes>
+        </BrowserRouter>
 
         <footer className="text-center py-6 text-sm text-gray-400 mt-90">
           Made by Jisu Kim © 2025
