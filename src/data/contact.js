f6cd7acfa5cda581633a5a1b72f9922fb129eb6f
JISU KIM <97648027+jijisusu3@users.noreply.github.com>
2025-08-13 14:40:58 -0400

    Edit img path

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,6 @@
 export const contactItems = [
   {
-    icon: "/contact/linkedin.svg",
+    icon: "https://jijisusu3.github.io/donggu-lee-portfolio/contact/linkedin.svg",
     label: "LinkedIn",
     value: "@donggu-lee-DL",
     link: "https://www.linkedin.com/in/donggu-lee-DL",
@@ -12,18 +12,18 @@ export const contactItems = [
     link: "https://scholar.google.com/citations?user=nBGrpW8AAAAJ&hl=en",
   },
   {
-    icon: "/contact/email.svg",
+    icon: "https://jijisusu3.github.io/donggu-lee-portfolio/contact/email.svg",
     label: "Email",
     value: "dlee42@ncsu.edu",
     link: "mailto:dlee42@ncsu.edu",
   },
   {
-    icon: "/contact/phone.svg",
+    icon: "https://jijisusu3.github.io/donggu-lee-portfolio/contact/phone.svg",
     label: "Phone",
     value: "+1 (919) 353-1502",
   },
   {
-    icon: "/contact/map.svg",
+    icon: "https://jijisusu3.github.io/donggu-lee-portfolio/contact/map.svg",
     label: "Location",
     value: "North Carolina, USA",
   },
